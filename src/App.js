84678cec33dd7d5c1aed9df9a71153cfb69a18ad
KirkWest <126360978+KirkWest@users.noreply.github.com>
2023-09-21 20:16:31 +1000

    businesscardparent and display made

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import './App.css';
+import BusinessCardParent from "./BusinessCardParent";
+
+function App() {
+  return (
+    <div className="App">
+        <BusinessCardParent />
+
+        <BusinessCardParent />
+        <BusinessCardParent />
+        <BusinessCardParent />
+        <BusinessCardParent />
+        <BusinessCardParent />
+        <BusinessCardParent />
+        <BusinessCardParent />
+        <BusinessCardParent />
+        <BusinessCardParent />
+    </div>
+  );
+}
+
+export default App;
